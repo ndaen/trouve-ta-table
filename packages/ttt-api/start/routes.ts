3a427c2c23669d1fc0e2c10350581e400584bb73
Noah Daën <daennoah@gmail.com>
2025-07-10 22:41:24 +0200

    feat: add projects routes and controller

--- packages/ttt-api/start/routes.ts
+++ packages/ttt-api/start/routes.ts
@@ -2,6 +2,7 @@ import router from '@adonisjs/core/services/router'
 import AuthController from "#controllers/auth_controller";
 import {middleware} from "#start/kernel";
 import UsersController from '#controllers/users_controller';
+import ProjectsController from "#controllers/projects_controller";
 
 router.group(() => {
   router.post('/auth/register', [AuthController, 'register'])
@@ -29,3 +30,15 @@ router.group(() => {
   router.patch('/:id', [UsersController, 'update'])
   router.delete('/:id', [UsersController, 'delete'])
 }).prefix('/api/users').middleware(middleware.auth())
+
+
+router.group(() => {
+  router.get('/', [ProjectsController, 'index'])
+  router.get('/:id', [ProjectsController, 'show'])
+  router.post('/', [ProjectsController, 'create'])
+  router.patch('/:id', [ProjectsController, 'update'])
+  router.delete('/:id', [ProjectsController, 'delete'])
+  router.group(() => {
+    router.get('/all', [ProjectsController, 'showByUser'])
+  }).prefix('/user')
+}).prefix('/api/projects').middleware(middleware.auth())
