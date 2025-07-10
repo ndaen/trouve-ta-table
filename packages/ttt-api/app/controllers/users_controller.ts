020c6af74b19a3417feecd85ef6c7185342c9e7c
Noah Daën <daennoah@gmail.com>
2025-07-10 21:39:52 +0200

    feat: add users routes to get all, get by id, update and delete

--- packages/ttt-api/app/controllers/users_controller.ts
+++ packages/ttt-api/app/controllers/users_controller.ts
@@ -8,9 +8,39 @@ export default class UsersController {
   }
 
   public async show({ params, response }: HttpContext) {
-
     const userId = params.id
-    response.json({ message: `User details for ID: ${userId}` })
+    const user = await User.findBy('id', userId);
+    if (!user) {
+      return response.status(404).json({ message: 'User not found' })
+    }
+    response.json({ message: `User details for ID: ${userId}`, user })
+  }
+  public async update({ params, request, response }: HttpContext) {
+    const userId = params.id
+    const user = await User.findBy('id', userId);
+    if (!user) {
+      return response.status(404).json({ message: 'User not found' })
+    }
+
+    const payload = request.body()
+    user.merge(payload)
+    await user.save()
+
+    response.json({ message: `User updated successfully`, user })
   }
 
+  public async delete({ params, response, auth }: HttpContext) {
+    const userId = params.id
+    const user = await User.findBy('id', userId);
+    if (!user) {
+      return response.status(404).json({ message: 'User not found' })
+    }
+
+    if (user.role === 'admin' && auth.user?.role !== 'admin') {
+      return response.status(403).json({ message: 'Cannot delete an admin user' })
+    }
+
+    await user.delete()
+    response.json({ message: `User with ID: ${userId} deleted successfully` })
+  }
 }
--- packages/ttt-api/start/routes.ts
+++ packages/ttt-api/start/routes.ts
@@ -1,6 +1,7 @@
 import router from '@adonisjs/core/services/router'
 import AuthController from "#controllers/auth_controller";
 import {middleware} from "#start/kernel";
+import UsersController from '#controllers/users_controller';
 
 router.group(() => {
   router.post('/auth/register', [AuthController, 'register'])
@@ -21,3 +22,10 @@ router.group(() => {
     router.get('/me', [AuthController, 'me'])
   }).prefix('/auth').middleware(middleware.auth())
 }).prefix('/api')
+
+router.group(() => {
+  router.get('/', [UsersController, 'index'])
+  router.get('/:id', [UsersController, 'show'])
+  router.patch('/:id', [UsersController, 'update'])
+  router.delete('/:id', [UsersController, 'delete'])
+}).prefix('/api/users').middleware(middleware.auth())
