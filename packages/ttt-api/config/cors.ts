7978f9f27fd5a20216cbd8b64b01b6c549aa2a83
Noah Daën <daennoah@gmail.com>
2025-08-29 19:50:16 +0200

    add cors config

--- packages/ttt-api/config/cors.ts
+++ packages/ttt-api/config/cors.ts
@@ -8,7 +8,13 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
     enabled: true,
-    origin: ['http://localhost:5173', 'http://localhost:3333', 'http://localhost'],
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:3333',
+        'http://localhost',
+        'http://31.97.155.246',
+        'http://31.97.155.246:80',
+    ],
     methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     headers: true,
     exposeHeaders: [],
